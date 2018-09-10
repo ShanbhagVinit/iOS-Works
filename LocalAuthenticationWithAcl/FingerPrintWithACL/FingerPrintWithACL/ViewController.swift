599e6ddb842299cb150eb9db3e50b001a418e99b
Vinit Shanbhag <Vinit.Shanbagh@tesco.com>
2018-09-10 20:59:51 +0530

    Adding new project with the screen loading

---
+++ LocalAuthenticationWithAcl/FingerPrintWithACL/FingerPrintWithACL/ViewController.swift
@@ -0,0 +1,32 @@
+//
+//  ViewController.swift
+//  FingerPrintWithACL
+//
+//  Created by Vinit Shanbhag on 10/09/18.
+//  Copyright © 2018 Vinit Shanbhag. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        let button = UIButton(type: .custom)
+        button.frame = CGRect(x: 0, y: 0, width: 80, height: 40)
+        button.setTitle("Tap To Login", for: .normal)
+        button.setTitleColor(.blue, for: .normal)
+        view.addSubview(button)
+        button.addTarget(self, action: #selector(didTapLogin), for: .touchUpInside)
+        button.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
+        button.centerYAnchor.constraint(equalTo: view.centerYAnchor).isActive = true
+        button.translatesAutoresizingMaskIntoConstraints = false
+        
+    }
+    
+    @objc private func didTapLogin() {
+        print("didTapLogin")
+    }
+
+}
+
